40e5752f7112d6063c25536313f971fad688c33d
umutates <umutates@DESKTOP-J93R766.mynet>
2018-08-06 08:00:19 +0300

    unused class clean

--- src/test/java/com/shoppingcart/TyshoppingcartApplicationTests.java
+++ src/test/java/com/shoppingcart/TyshoppingcartApplicationTests.java
@@ -2,24 +2,17 @@ package com.shoppingcart;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.shoppingcart.repository.IProductRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TyshoppingcartApplicationTests {
     
-	@Autowired IProductRepository productRepository;
-	
 	
 	@Test
 	public void contextLoads() {
-//		Category category=new Category("FOOD");
-//		Product product=new Product("ProteinBar",new BigDecimal(10), category);
-//		productRepository.save(product);
+
 	}
 
 }
